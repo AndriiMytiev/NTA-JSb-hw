f35337cc383449a88829e3574b697e70aae67cb6
AndriiMytiev <79248454+AndriiMytiev@users.noreply.github.com>
2022-05-12 00:49:09 +0300

    Update task12.js

--- js-core-3/js/task12.js
+++ js-core-3/js/task12.js
@@ -2,9 +2,13 @@ const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 
 let password = prompt("Введите пароль: ");
+
 if (password == null){
     message = 'Отменено пользователем!';
 } else if(password == ADMIN_PASSWORD){
     message = 'Добро пожаловать!';
-}
+} else {
+    message = 'Не верный пароль!';
+};
+
 alert(message);
