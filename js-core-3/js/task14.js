f4e51542c439c2f73e37bb84ca827a96c15fabfc
AndriiMytiev <79248454+AndriiMytiev@users.noreply.github.com>
2022-05-12 16:33:29 +0300

    Update task14.js

--- js-core-3/js/task14.js
+++ js-core-3/js/task14.js
@@ -5,15 +5,15 @@ let countryArray = ["Китай", "Чили", "Австралия", "Индия"
 let priceArray = [100, 250, 170, 80, 120];
 
 switch(normalizeUserCountry){
-    case countryArray[0].toLowerCase(): console.log(`Доставка в ${countryArray[0]} будет стоить ${priceArray[0]} кредитов.`);
+    case countryArray[0].toLowerCase(): alert(`Доставка в ${countryArray[0]} будет стоить ${priceArray[0]} кредитов.`);
     break;
-    case countryArray[1].toLowerCase(): console.log(`Доставка в ${countryArray[1]} будет стоить ${priceArray[1]} кредитов.`);
+    case countryArray[1].toLowerCase(): alert(`Доставка в ${countryArray[1]} будет стоить ${priceArray[1]} кредитов.`);
     break;
-    case countryArray[2].toLowerCase(): console.log(`Доставка в ${countryArray[2]} будет стоить ${priceArray[2]} кредитов.`);
+    case countryArray[2].toLowerCase(): alert(`Доставка в ${countryArray[2]} будет стоить ${priceArray[2]} кредитов.`);
     break;
-    case countryArray[3].toLowerCase(): console.log(`Доставка в ${countryArray[3]} будет стоить ${priceArray[3]} кредитов.`);
+    case countryArray[3].toLowerCase(): alert(`Доставка в ${countryArray[3]} будет стоить ${priceArray[3]} кредитов.`);
     break;
-    case countryArray[4].toLowerCase(): console.log(`Доставка в ${countryArray[4]} будет стоить ${priceArray[4]} кредитов.`);
+    case countryArray[4].toLowerCase(): alert(`Доставка в ${countryArray[4]} будет стоить ${priceArray[4]} кредитов.`);
     break;
     default: alert("В вашей стране доставка не доступна");
 };
