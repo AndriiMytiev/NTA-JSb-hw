5e6b4a57ddade3befdc9d6f3f70616a092816503
AndriiMytiev <79248454+AndriiMytiev@users.noreply.github.com>
2022-05-25 21:08:59 +0300

    Request-form-part1

---
+++ js-core-7/index.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("#program-request");
+
+form.addEventListener("input", (event)=>{
+    if (event.target.id === 'email') {
+        checkValue(event.target, '^[A-z]+@([A-z][-A-z]+.)+[A-z]{2,4}$');
+    };
+})
+function checkValue(input, pattern){
+    const regex = new RegExp(pattern);
+    let check = input.value.match(regex);
+    if(!check){
+        input.style.border = 'solid red';
+    } else input.style.border = 'ridge white'
+}
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const request = {
+        name: event.target.elements.name.value,
+        phone: event.target.elements.phone.value,
+        address: event.target.elements.address.value,
+        email: event.target.elements.email.value,
+        serviceType: event.target.elements.serviceType.value,
+        date: event.target.elements.date.value,
+        comment: event.target.elements.comment.value
+    };
+    if(!request.name || !request.phone || !request.address || !request.email || 
+        !request.serviceType || !request.date){
+          alert("Please fill in the required fields")  
+    } else {
+        console.log('request:', request);
+    }
+    return request;
+})
