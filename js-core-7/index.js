6d30cf21f1f06884a962667efb3722e554135e4d
AndriiMytiev <79248454+AndriiMytiev@users.noreply.github.com>
2022-05-26 13:50:46 +0300

    done

--- js-core-7/index.js
+++ js-core-7/index.js
@@ -1,20 +1,22 @@
 const form = document.querySelector("#program-request");
+const requests = [];
 
 form.addEventListener("input", (event)=>{
-    if (event.target.id === 'email') {
+    if (event.target.id === 'email'){
         checkValue(event.target, '^[A-z]+@([A-z][-A-z]+.)+[A-z]{2,4}$');
     };
-})
+});
+
 function checkValue(input, pattern){
     const regex = new RegExp(pattern);
     let check = input.value.match(regex);
     if(!check){
-        input.style.border = 'solid red';
-    } else input.style.border = 'ridge white'
-}
+        input.style.border = '3px ridge red';
+    } else input.style.border = '3px ridge';
+};
 
 form.addEventListener("submit", (event)=>{
-    event.preventDefault()
+    event.preventDefault();
     const request = {
         name: event.target.elements.name.value,
         phone: event.target.elements.phone.value,
@@ -22,13 +24,15 @@ form.addEventListener("submit", (event)=>{
         email: event.target.elements.email.value,
         serviceType: event.target.elements.serviceType.value,
         date: event.target.elements.date.value,
-        comment: event.target.elements.comment.value
+        comment: event.target.elements.comment.value,
     };
     if(!request.name || !request.phone || !request.address || !request.email || 
         !request.serviceType || !request.date){
-          alert("Please fill in the required fields")  
+          alert("Please fill in the required fields");  
     } else {
         console.log('request:', request);
-    }
+        requests.push(request);
+        localStorage.setItem("requests", JSON.stringify(requests));
+    };
     return request;
-})
+});
